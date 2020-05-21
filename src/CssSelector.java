cdd0917cb3ba78a868a09a6c33bf201336310297
Serdar <serdar2225@gmail.com>
2020-05-21 02:17:41 -0400

    cssSelector exe "Normal warning message"

--- src/CssSelector.java
+++ src/CssSelector.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class CssSelector {
@@ -9,6 +11,21 @@ public class CssSelector {
         driver.get("https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html");
         driver.manage().window().maximize();
 
+        driver.findElement(By.cssSelector("#normal-btn-warning")).click();
+
+        WebElement message = driver.findElement(By.cssSelector(".alert-normal-warning"));
+
+        WebElement button = driver.findElement(By.cssSelector(".alert-normal-warning > button"));
+
+        String text = message.getText().replace(button.getText(), "").trim();
+
+        String test = "I'm a normal warning message. To close use the appropriate button.";
+
+        System.out.println( test.equals( text ) ? "Success!" : "Failure!" );
+
+        button.click();
+        System.out.println( !message.isDisplayed() ? "Success!" : "Failure" );
+        driver.quit();
 
     }
 
--- src/XPath.java
+++ src/XPath.java
@@ -4,9 +4,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class XPath {
-    // repeat Task 3 for all the buttons. Success, warning, danger and info buttons.
     // https://www.seleniumeasy.com/test/bootstrap-alert-messages-demo.html
-    // click on the "Normal success message" button then validate
+    // click on the "Normal warning message" button then validate
     // that message is equal "I'm a normal success message. To close use the appropriate button."
     // then click on X button on the message
     // validate the the message is invisible or not present
