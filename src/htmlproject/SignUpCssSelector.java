b3861e766a6d40fa181770195346e5dc5d4d20f3
Serdar <serdar2225@gmail.com>
2020-05-23 10:59:12 -0400

    signupform CssSelectors version

---
+++ src/htmlproject/SignUpCssSelector.java
@@ -0,0 +1,29 @@
+package htmlproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SignUpCssSelector {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///C:/Users/suler/IdeaProjects/Xpath%20and%20Css%20Selector%20Practice/src/htmlproject/SignUpForm.html");
+
+
+
+        driver.findElement(By.cssSelector("#username")).sendKeys("avangarde2225");
+        driver.findElement(By.cssSelector("#password")).sendKeys("1234567");
+        driver.findElement(By.cssSelector("#passwordrepeat")).sendKeys("1234567");
+        driver.findElement(By.cssSelector("#address")).sendKeys("350 5th ave, New York,NY, 10005");
+        driver.findElement(By.cssSelector("#male")).click();
+        driver.findElement(By.cssSelector("#option2")).click();
+        driver.findElement(By.cssSelector("#submit")).click();
+
+        driver.quit();
+
+    }
+}
