514829e10f2aeab4845d4ba8ba4bf6791823b3b4
Serdar <serdar2225@gmail.com>
2020-05-23 10:51:46 -0400

    signupform xpath version

---
+++ src/htmlproject/SignUpXpath.java
@@ -0,0 +1,30 @@
+package htmlproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class SignUpXpath {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///C:/Users/suler/IdeaProjects/Xpath%20and%20Css%20Selector%20Practice/src/htmlproject/SignUpForm.html");
+
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("avangarde2225");
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("1234567");
+        driver.findElement(By.xpath("//input[@id='passwordrepeat']")).sendKeys("1234567");
+        driver.findElement(By.xpath("//textarea[@id='address']")).sendKeys("350 5th ave, New York,NY, 10005");
+        driver.findElement(By.xpath("//input[@id='male']")).click();
+        driver.findElement(By.xpath("//*[@name='Plan']//child::option[1]")).click();
+        driver.findElement(By.xpath("//button[@id='submit']")).click();
+
+        driver.quit();
+
+    }
+}
