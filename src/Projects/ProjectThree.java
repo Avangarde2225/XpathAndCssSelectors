fe39d1f6ce2408cab5be8f2bf4bb4a814d8c3553
Serdar <serdar2225@gmail.com>
2020-05-26 01:05:27 -0400

    project 3 validation

--- src/Projects/ProjectThree.java
+++ src/Projects/ProjectThree.java
@@ -1,7 +1,11 @@
 package Projects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class ProjectThree {
     /**
@@ -17,5 +21,18 @@ public class ProjectThree {
         driver.get("https://ultimateqa.com/automation/fake-pricing-page/");
 
         driver.manage().window().maximize();
+
+        List<WebElement>priceSums = driver.findElements(By.xpath("//*[@class='et_pb_sum']"));
+
+        String [] prices = {"$0", "$80", "$900"};
+
+        for (int i = 0; i < prices.length; i++) {
+            if(priceSums.get(i).getText().equals(prices[i])){
+                System.out.println("Total is " + priceSums.get(i).getText());
+            } else{
+                System.out.println("Failed total is " + priceSums.get(i).getText());
+            }
+        }
+        driver.quit();
     }
 }
