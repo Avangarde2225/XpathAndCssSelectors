1b9e24ad5f0b0248179192b96169bca418f1b9f5
Serdar <serdar2225@gmail.com>
2020-05-29 18:24:21 -0400

    url parsing with HashMap

--- src/htmlproject/UrlParsin.java
+++ src/htmlproject/UrlParsin.java
@@ -3,37 +3,47 @@ package htmlproject;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class UrlParsin {
-    public static void main(String[] args) throws MalformedURLException {
-
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
+    public static void main(String[] args) {
+        System.setProperty( "webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe" );
         WebDriver driver = new ChromeDriver();
-        driver.get("file:///C:/Users/suler/IdeaProjects/Xpath%20and%20Css%20Selector%20Practice/src/htmlproject/SignUpForm.html");
+        String path = "file:///C:/Users/suler/IdeaProjects/Xpath%20and%20Css%20Selector%20Practice/src/htmlproject/SignUpForm.html";
+        driver.get( path );
 
+        driver.findElement( By.name( "username" ) ).sendKeys( "name" );
+        driver.findElement( By.name( "password" ) ).sendKeys( "password" );
+        driver.findElement( By.name( "password_repeat" ) ).sendKeys( "password" );
+        driver.findElement( By.name( "address" ) ).sendKeys( "7514 Riverview Rd.\n" + "Sheboygan, WI 53081" );
+        driver.findElement( By.cssSelector( "input[type='radio'][value='male']" ) ).click();
+        Select plan = new Select( driver.findElement( By.name( "plan" ) ) );
+        plan.selectByIndex( plan.getOptions().size() - 1 ); //selects last option
+        driver.findElement( By.name( "agreement" ) ).click();
+        driver.findElement( By.id( "submit" ) ).click();
+        String currentUrl = driver.getCurrentUrl();
 
-        driver.findElement(By.cssSelector("#username")).sendKeys("avangarde2225");
-        driver.findElement(By.cssSelector("#password")).sendKeys("1234567");
-        driver.findElement(By.cssSelector("#passwordrepeat")).sendKeys("1234567");
-        driver.findElement(By.cssSelector("#address")).sendKeys("350 5th ave, New York,NY, 10005");
-        driver.findElement(By.cssSelector("#male")).click();
-        driver.findElement(By.cssSelector("#option2")).click();
-        driver.findElement(By.cssSelector("#submit")).click();
-
-        String url = driver.getCurrentUrl();
-        System.out.println(url);
-
-        parseUrl(driver, url);
-    }
-
-    public static void parseUrl(WebDriver driver, String currentUrl) throws MalformedURLException {
-        URL myUrl = new URL(currentUrl);
-        String sentKeys = myUrl.getQuery();
-        for (String str : sentKeys.split("&")) {
-            System.out.println(str);
+        Map<String, String> queryMap = getQueryMap( currentUrl );
+        for(String key : queryMap.keySet()) {
+            System.out.println( key + " = " + queryMap.get( key ) );
         }
+//        driver.quit();
+    }
+    //SignUpForm.html?username=name&password=password&password_repeat=password&address=7514+Riverview+Rd.%0D%0ASheboygan%2C+WI+53081&sex=male&plan=enterprise&agreement=on&x=100&y=24
+    public static Map<String, String> getQueryMap(String query) {
+        String onlyParams = query.substring( query.indexOf( "?" ) + 1 );
+        String[] params = onlyParams.split( "&" );
+        Map<String, String> map = new HashMap<String, String>();
+        for(String param : params) {
+            String key = param.split( "=" )[0];
+            String value = param.split( "=" )[1];
+            map.put( key, value );
+        }
+        return map;
     }
 }
