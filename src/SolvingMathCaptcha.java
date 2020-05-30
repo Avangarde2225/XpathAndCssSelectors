f14ae187b2abb4a27d77b338a651527385c6a3cb
Serdar <serdar2225@gmail.com>
2020-05-30 02:45:54 -0400

    captcha code reusable

--- src/SolvingMathCaptcha.java
+++ src/SolvingMathCaptcha.java
@@ -21,8 +21,26 @@ public class SolvingMathCaptcha {
         driver.findElement(By.xpath("//input[@id='et_pb_contact_name_1']")).sendKeys("Tom");
         driver.findElement(By.xpath("//textarea[@name='et_pb_contact_message_1']")).sendKeys("bla bla bla text");
 
+        String mathCapthca = driver.findElement(By.xpath("//div[@class='et_contact_bottom_container']/div/p/span")).getText().trim();
+        //System.out.println(mathCapthca);
+        String spaceRemoved = mathCapthca.replaceAll("\\s+","");
 
+        //get two numbers
+        String[] twoNumbers = spaceRemoved.split("\\+");
+//        for (String num : twoNumbers) {
+//            System.out.println(num);
+//        }
 
+        String part1 = twoNumbers[0];
+        String part2 = twoNumbers[1];
+
+        int sumOfTwoNums = Integer.valueOf(part1) + Integer.valueOf(part2);
+
+        driver.findElement(By.xpath("//*[@class='input et_pb_contact_captcha']")).sendKeys(sumOfTwoNums+ "");
+
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+        driver.quit();
     }
 }
 
