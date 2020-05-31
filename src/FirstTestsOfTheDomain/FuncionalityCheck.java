e7b109356f7d651026926a68c925a40d4c2c77c6
Serdar <serdar2225@gmail.com>
2020-05-30 21:18:28 -0400

    Username and password of the test environment

--- src/FirstTestsOfTheDomain/FuncionalityCheck.java
+++ src/FirstTestsOfTheDomain/FuncionalityCheck.java
@@ -1,7 +1,10 @@
 package FirstTestsOfTheDomain;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class FuncionalityCheck {
     public static void main(String[] args) {
@@ -9,6 +12,14 @@ public class FuncionalityCheck {
         WebDriver driver = new ChromeDriver();
         driver.get("https://basqar.techno.study/");
         driver.manage().window().maximize();
+
+        driver.findElement(By.cssSelector("input[placeholder='Username']")).sendKeys("nigeria_tenant_admin");
+        driver.findElement(By.cssSelector("[formcontrolname=\"password\"]")).sendKeys("TnvLOl54WxR75vylop2A");
+        driver.findElement(By.cssSelector("button[aria-label=\"LOGIN\"]")).click();
+
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+
 
     }
 }
